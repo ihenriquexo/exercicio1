825cacb2165646b99e52588a975b27b084aeb356
ihenriquex <ihenriquexo@gmail.com>
2023-07-17 15:53:23 -0300

    terminei o contador de notas

--- index.js
+++ index.js
@@ -5,19 +5,28 @@ var notasDe1=1
 
 var valor = prompt("digite o valor")
 
+console.log("a quantidade de notas de 100 é "+
+Math.floor(valor/100)
++" e a sobra é "+
+Math.floor(valor%100))
 
 
-if (quantNotasD100=console.log(Math.floor(valor/notasDe100))) {
+
+if (quantNotasD100=console.log(Math.floor(valor/100))) {
   
-}if(quantNotasD50=console.log(Math.floor(valor/notasDe50))) {
+}if(quantNotasD50=console.log(Math.floor(valor/50))) {
   
-}if(quantNotasD10=console.log(Math.floor(valor/notasDe10))) {
+}if(quantNotasD10=console.log(Math.floor(valor/10))) {
   
-}if(quantNotasD1=console.log(Math.floor(valor/notasDe1))) {
+}if(quantNotasD1=console.log(Math.floor(valor/1))) {
   
 }
 
-console.log(parseInt("você me enviou",quantNotasD100,"notas de 100",quantNotasD50,"notas de 50",quantNotasD10,"notas de 10 e ",quantNotasD1," notas de 1"))
+console.log(parseInt("você me enviou",valor,
+" que é ",quantNotasD100," notas de 100 ",
+quantNotasD50," notas de 50 ",
+quantNotasD10," notas de 10 e ",
+quantNotasD1," notas de 1"))
 
 
 
--- test.js
+++ test.js
@@ -1,2 +1,12 @@
-var valor=prompt("digite o valor")
-console.log("a quantidade de notas de 100 é "+Math.floor(valor/100)+" e a sobra é "+(Math.floor(valor%100)))
+
+var valor = prompt("digite o valor")
+var sobra=Math.floor(valor%100)
+var sobra1=Math.floor(sobra%50)
+var sobra2=Math.floor(sobra1%10)
+var sobra3=Math.floor(sobra2%1)
+console.log(valor+", que dá "+Math.floor(valor/100)+" notas de 100, "+Math.floor(sobra/50)
++" notas de 50, "+Math.floor(sobra1/10)+" notas de 10 e "
++Math.floor(sobra2/1)+" notas de 1")
+
+
+
