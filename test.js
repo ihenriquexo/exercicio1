c4e05aa50e06bdfb17d7f49c72ac10eb11f0273b
ihenriquex <ihenriquexo@gmail.com>
2023-07-17 13:09:14 -0300

    entendi a solução

--- test.js
+++ test.js
@@ -1,5 +1,2 @@
 var valor=prompt("digite o valor")
-var quant=console.log(Math.floor(valor/100))
-var sobra=console.log(Math.floor(valor%100))
-
-console.log("a quantidade de notas de 100 é"+quant+"e a sobra é"+sobra)
+console.log("a quantidade de notas de 100 é "+Math.floor(valor/100)+" e a sobra é "+(Math.floor(valor%100)))
