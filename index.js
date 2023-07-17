cc025664c0a538482fc50b699880f369888caf01
ihenriquex <ihenriquexo@gmail.com>
2023-07-17 13:00:42 -0300

    metade do exercicio 1

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ var notasDe100=100
 var notasDe50=50
 var notasDe10=10
 var notasDe1=1
+
+var valor = prompt("digite o valor")
+
+
+
+if (quantNotasD100=console.log(Math.floor(valor/notasDe100))) {
+  
+}if(quantNotasD50=console.log(Math.floor(valor/notasDe50))) {
+  
+}if(quantNotasD10=console.log(Math.floor(valor/notasDe10))) {
+  
+}if(quantNotasD1=console.log(Math.floor(valor/notasDe1))) {
+  
+}
+
+console.log(parseInt("você me enviou",quantNotasD100,"notas de 100",quantNotasD50,"notas de 50",quantNotasD10,"notas de 10 e ",quantNotasD1," notas de 1"))
+
+
+
---
+++ test.js
@@ -0,0 +1,5 @@
+var valor=prompt("digite o valor")
+var quant=console.log(Math.floor(valor/100))
+var sobra=console.log(Math.floor(valor%100))
+
+console.log("a quantidade de notas de 100 é"+quant+"e a sobra é"+sobra)
