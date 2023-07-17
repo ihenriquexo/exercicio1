5b67362e49b8f4903af093206faa7116bf5989a3
ihenriquex <ihenriquexo@gmail.com>
2023-07-17 11:16:45 -0300

    criando separador de notas

---
+++ index.js
@@ -0,0 +1,13 @@
+var totalAlunos = 10;
+var somaNotas = 0;
+var homens = 0;
+var mulheresNotaAcima7 = 0;
+var maiorNotaHomem = 0;
+
+for (var i = 1; i <= totalAlunos; i++) {
+  var nota = parseFloat(prompt("Digite a nota (0 a 10) do aluno " + i + ":"));
+  var sexo = prompt("Digite o sexo (M ou F) do aluno " + i + ":");
+  somaNotas<=nota
+  
+
+}
